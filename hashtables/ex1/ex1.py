61d0f3697e9adfef88033f5346ea2e64a255a573
D-Fink <dfink4badjuju@gmail.com>
2020-05-08 11:28:44 -0600

    mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
-
     """
     YOUR CODE HERE
     """
+    weight_dict = {}
+
+    #iterate through the length of weights
+    for i in range(length):
+        #calculate if current weight matches remainder of any weights in dict
+        calc = weight_dict.get(limit - weights[i])
+        # if calculate finds match, return the indexes
+        if calc is not None:
+            return (i, calc)
+        #if calculate returns none, assign weight an index and add to dict
+        else:
+            weight_dict[weights[i]] = i
 
     return None
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,29 @@
 def intersection(arrays):
-
     """
     YOUR CODE HERE
     """
+    count = {}
+    # list to results to
+    result = []
+    # iterate through our array with the index and array at index *use enumerate*
+    for i, nums in enumerate(arrays):
+        # iterate through individual arrays
+        for j in nums:
+            # if not our first array and key exists, increase count.
+            if count.get(j) is not None and i > 0:
+                count[j] = count[j] + 1
+            # if first in array set each number in array to 1
+            elif count.get(j) is None and i == 0:
+                count[j] = 1
+            # otherwise dont do anything
+            else:
+                continue
+    
+    # iterate through our dict
+    for num in count:
+        # check to see if count matches the amount of arrays checked and append it to our list
+        if count[num] == len(arrays):
+            result.append(num)
 
     return result
 
